72e01d1d45f97c6f1e073b462bcd464f895f16a2
Rasmus Anker <info@rasmusanker.com>
2017-01-26 21:03:42 +0000

    creates mockup devices dynamically

---
+++ source/js/add_project.js
@@ -0,0 +1,231 @@
+function addProject(section, scene) {
+	var loader = new THREE.JSONLoader();
+	
+	var screenMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });	
+	var video = $(section).find("video")[0];
+
+	var videoTexture = new THREE.VideoTexture( video );
+	videoTexture.minFilter = THREE.LinearFilter;
+	videoTexture.magFilter = THREE.LinearFilter;
+	videoTexture.format = THREE.RGBFormat;
+	videoTexture.wrapS = THREE.ClampToEdgeWrapping;
+	videoTexture.wrapT = THREE.ClampToEdgeWrapping;
+	screenMaterial.map = videoTexture;
+
+	var glassMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff  });
+	glassMaterial.specular.r=1;
+	glassMaterial.specular.g=1;
+	glassMaterial.specular.b=1;
+	glassMaterial.transparent = true;
+	glassMaterial.opacity = 0.1;
+	glassMaterial.shininess = 0.2;
+
+	if ($(section).hasClass("mobile")) {
+		var phoneGroup = new THREE.Group();
+		var screenGeometry = new THREE.PlaneGeometry( 0.55, 0.983, 1 );
+		var screen = new THREE.Mesh( screenGeometry, screenMaterial );
+		screen.receiveShadow = false;
+		screen.position.z = 0.0292;
+		screen.position.y = 0.05;
+		screen.receiveShadow = false;
+		screen.position.z = 0.0292;
+		screen.position.y = 0.05;
+		phoneGroup.add( screen );
+
+		var glass = new THREE.Mesh( screenGeometry, glassMaterial );
+		glass.receiveShadow = true;
+		glass.position.z = 0.0293;
+		glass.position.y = 0.05;
+		glass.transparent = true;		
+		// phoneGroup.add(glass);
+
+		loader.load(
+			// resource URL
+			'models/phone.json',
+			// Function when resource is loaded
+			function ( geometry, materials ) {
+				var material = new THREE.MultiMaterial( materials );
+				var object = new THREE.Mesh( geometry, material );				
+				object.position.y=-0.6;
+				object.castShadow=true;
+				object.traverse( function ( child ) {
+		        if ( child instanceof THREE.Mesh ) {
+		            child.geometry.computeVertexNormals();
+		        }
+		        phoneGroup.position.y=-5;
+		        phoneGroup.position.z=-1;
+		        for (i=0;i<object.material.materials.length;i++) {
+		        	var tC = .75;
+		        	object.material.materials[i].side=2;
+					object.material.materials[i].specular.r=tC;
+					object.material.materials[i].specular.g=tC;
+					object.material.materials[i].specular.b=tC;	
+					object.material.materials[i].emissive.r=.0;
+					object.material.materials[i].emissive.g=.0;
+					object.material.materials[i].emissive.b=.0;
+					object.material.materials[i].shininess=10;	
+		        } 
+				phoneGroup.add( object );
+		    });
+
+			phoneObject = object;
+			phoneGroup.position.z=0;
+			phoneGroup.position.y=-1;
+			phoneGroup.scale.set( 25, 25, 25 );
+			phoneGroup.castShadow = true;
+
+			// phone1 = phoneGroup.clone();
+			// phone1.position.x=-25;
+			// phone1.position.z = -20;
+			
+			TweenMax.to(phoneGroup.rotation, 1.5, {y:de2ra(0),  ease:Power4.easeOut});
+			var tween = TweenMax.to(phoneGroup.position, 3.5,{ y:0, yoyo:true, repeat:-1, ease:Power2.easeInOut, onComplete:function() {}});
+			// setTimeout(function() {
+			// 	var tween = TweenMax.to(phone1.position, 3.5,{ y:0, yoyo:true, repeat:-1, ease:Power2.easeInOut, onComplete:function() {}});
+			// },1500);
+			
+			
+		});
+		scene.add( phoneGroup );
+	} else if ($(section).hasClass("desktop")) {
+		var macbookGroup = new THREE.Group();
+		var macbookScreenGroup = new THREE.Object3D();
+		loader.load(
+			// resource URL
+			'models/screen.json',
+			// Function when resource is loaded
+			function ( geometry, materials ) {
+				var material = new THREE.MultiMaterial( materials );
+				var object = new THREE.Mesh( geometry, material );
+				// scene.add( object );
+				
+				object.position.y=0;
+				object.position.x=-0.5;
+				object.position.z=0;
+				// object.rotation.y=de2ra(180);
+				
+				object.castShadow=true;
+				object.traverse( function ( child ) {
+		        if ( child instanceof THREE.Mesh ) {
+		            child.geometry.computeVertexNormals();
+		        }
+		        for (i=0;i<object.material.materials.length;i++) {
+		        	var tC = .75;
+		        	object.material.materials[i].side=2;
+					object.material.materials[i].specular.r=tC;
+					object.material.materials[i].specular.g=tC;
+					object.material.materials[i].specular.b=tC;	
+					object.material.materials[i].emissive.r=.0;
+					object.material.materials[i].emissive.g=.0;
+					object.material.materials[i].emissive.b=.0;
+					object.material.materials[i].shininess=8;	
+		        }
+				macbookScreenGroup.add( object );
+		    });
+		});
+
+		var desktopGeometry = new THREE.PlaneGeometry( 8.7, 5.8, 1 );
+
+		var desktopScreen = new THREE.Mesh( desktopGeometry, screenMaterial );
+		desktopScreen.position.z = -3.16;
+		desktopScreen.position.y = 3.3;
+		desktopScreen.rotation.x = de2ra(1);
+
+		macbookScreenGroup.add(desktopScreen);
+
+		// var desktopGlass = new THREE.Mesh(desktopGeometry, glassMaterial);
+		// desktopGlass.receiveShadow = true;
+		// desktopGlass.position.z = -3;
+		// desktopGlass.position.y = 3.3;
+		// desktopGlass.rotation.x = de2ra(1);
+		// desktopGlass.transparent = false;
+		// macbookScreenGroup.add(desktopGlass);
+
+		var macbookPivot = new THREE.Object3D();
+		var tempgeometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
+		var tempmaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } );	
+		mesh = new THREE.Mesh( tempgeometry, tempmaterial );
+		macbookPivot.add( mesh );
+		
+		macbookScreenGroup.position.z=3;
+		macbookPivot.add(macbookScreenGroup);
+		macbookPivot.position.z=-2.8;
+		macbookPivot.rotation.x=de2ra(90);
+		var openLid = TweenMax.to(macbookPivot.rotation, 1, { x:0, ease:Power4.easeInOut});
+		var turnOnScreen = TweenMax.to(screenMaterial, 0.3, {opacity:1, delay:0.5});
+		macbookGroup.add(macbookPivot);
+
+		// macbookGroup.rotation.y=de2ra(90);
+		loader.load(
+			// resource URL
+			'models/keyboard.json',
+			// Function when resource is loaded
+			function ( geometry, materials ) {
+				var material = new THREE.MultiMaterial( materials );
+				var object = new THREE.Mesh( geometry, material );
+				// scene.add( object );
+				
+				object.position.y=0;
+				object.position.x=-0.5;
+				object.position.z=0;
+				
+				object.castShadow=true;
+				object.traverse( function ( child ) {
+		        if ( child instanceof THREE.Mesh ) {
+		            child.geometry.computeVertexNormals();
+		        }
+		        for (i=0;i<object.material.materials.length;i++) {
+		        	var tC = .75;
+		        	object.material.materials[i].side=2;
+					object.material.materials[i].specular.r=tC;
+					object.material.materials[i].specular.g=tC;
+					object.material.materials[i].specular.b=tC;	
+					object.material.materials[i].emissive.r=.0;
+					object.material.materials[i].emissive.g=.0;
+					object.material.materials[i].emissive.b=.0;
+					object.material.materials[i].shininess=1.5;	
+		        }
+				macbookGroup.add( object );
+		    });
+		});
+		loader.load(
+			// resource URL
+			'models/keys.json',
+			// Function when resource is loaded
+			function ( geometry, materials ) {
+				var material = new THREE.MultiMaterial( materials );
+				var object = new THREE.Mesh( geometry, material );
+				// scene.add( object );
+				
+				object.position.y=0;
+				object.position.x=-0.5;
+				object.position.z=0;
+				
+				object.castShadow=true;
+				object.traverse( function ( child ) {
+		        if ( child instanceof THREE.Mesh ) {
+		            child.geometry.computeVertexNormals();
+		        }
+		        for (i=0;i<object.material.materials.length;i++) {
+		        	var tC = .75;
+		        	object.material.materials[i].side=2;
+					object.material.materials[i].specular.r=tC;
+					object.material.materials[i].specular.g=tC;
+					object.material.materials[i].specular.b=tC;	
+					object.material.materials[i].emissive.r=.0;
+					object.material.materials[i].emissive.g=.0;
+					object.material.materials[i].emissive.b=.0;
+					object.material.materials[i].shininess=21;	
+		        }
+				macbookGroup.add( object );
+		    });
+		});	
+
+		macbookGroup.position.y=-10;
+		macbookGroup.position.z=10;
+		macbookGroup.scale.set(4,4,4);
+		// scene.add(macbookGroup);
+		var tween1 = TweenMax.to(macbookGroup.position, 3.5,{ y:-9, yoyo:true, repeat:-1, ease:Power2.easeInOut, onComplete:function() {}});
+		// var tween2 = TweenMax.to(macbookGroup.rotation, 3.5,{ y:de2ra(360), yoyo:true, repeat:-1, ease:Power2.easeInOut, onComplete:function() {}});
+	}		
+}
--- source/js/interface.js
+++ source/js/interface.js
@@ -25,7 +25,7 @@ $(function() {
 
 // if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 	var container;
-	var camera, scene, renderer, particles, geometry, materials = [], starMaterials = [], parameters, i, h, color, sprite, size, effect;
+	var camera, scene, renderer, particles, geometry, materials = [], parameters, i, h, color, sprite, size, effect;
 	var mouseX = 0, prevMouseX = 0, mouseY = 0, mouseDown = false, mouseDirection;
 	var phone = new Object(), tweenRotation, phoneObject;
 	var phoneObject, phoneGroup, newPhone, macbookGroup;
@@ -45,316 +45,33 @@ $(function() {
 		document.addEventListener( 'mousemove', onDocumentMouseMove, false ); 
 		container = document.createElement( 'div' );
 		container.setAttribute("class","content");
-		document.body.appendChild( container );
+		document.getElementsByClassName('devices')[0].appendChild( container );
 
-		camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 2000 );
-		camera.position.z = 65;
-		camera.position.y = 0;
-		// camera.lookAt( 0, 0, -65 );
-
-		scene = new THREE.Scene();
+		setEnvironment();
+		setLights();
 		
-		var loader = new THREE.JSONLoader();
-		phoneGroup = new THREE.Group();
-
-		var screenGeometry = new THREE.PlaneGeometry( 0.55, 0.983, 1 );
-		var mobileMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-
-		// var texture = THREE.ImageUtils.loadTexture( "css/screen.jpg" );
-		// texture.repeat.set( 1, 1 );	
-		// texture.anisotropy=2;
-
-		mobileVideo = document.getElementById( 'mobile' );
-		var mobileTexture = new THREE.VideoTexture( mobileVideo );
-		mobileTexture.minFilter = THREE.LinearFilter;
-		mobileTexture.magFilter = THREE.LinearFilter;
-		mobileTexture.format = THREE.RGBFormat;
-		mobileTexture.wrapS = THREE.ClampToEdgeWrapping;
-		mobileTexture.wrapT = THREE.ClampToEdgeWrapping;
-		// texture.anisotropy=2;
-
-		mobileMaterial.map = mobileTexture;
-		// screenMaterial.color = "#ffffff";
-
-		var screen = new THREE.Mesh( screenGeometry, mobileMaterial );
-		screen.receiveShadow = false;
-		// screen.rotation.x = de2ra(90);
-		screen.position.z = 0.0292;
-		screen.position.y = 0.05;
-
-
-
-
-		
-		
-		screen.receiveShadow = false;
-		// screen.rotation.x = de2ra(90);
-		screen.position.z = 0.0292;
-		screen.position.y = 0.05;
-
-		phoneGroup.add( screen );
-		
-		
-		var glassMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff  });
-		glassMaterial.specular.r=1;
-		glassMaterial.specular.g=1;
-		glassMaterial.specular.b=1;
-		glassMaterial.transparent = true;
-		glassMaterial.opacity = 0.1;
-		glassMaterial.shininess = 0.2;
-
-		var glass = new THREE.Mesh( screenGeometry, glassMaterial );
-		glass.receiveShadow = true;
-		glass.position.z = 0.0293;
-		glass.position.y = 0.05;
-		glass.transparent = true;
-
-
-		var groundGeometry = new THREE.PlaneGeometry( 250, 250, 1 );
-		var groundMaterial = new THREE.ShadowMaterial();
-
-		groundMaterial.opacity = .3;
-
-		var ground = new THREE.Mesh(groundGeometry, groundMaterial);
-		ground.receiveShadow = true;
-		ground.rotation.x=55;
-		ground.position.y=-20;
-		ground.transparent = true;
-
-		scene.add(ground);
-		phoneGroup.add(glass);
-
-		loader.load(
-			// resource URL
-			'models/phone.json',
-			// Function when resource is loaded
-			function ( geometry, materials ) {
-				var material = new THREE.MultiMaterial( materials );
-				var object = new THREE.Mesh( geometry, material );
-				// scene.add( object );
-				
-				object.position.y=-0.6;
-				object.castShadow=true;
-				object.traverse( function ( child ) {
-		        if ( child instanceof THREE.Mesh ) {
-		            child.geometry.computeVertexNormals();
-		        }
-		        phoneGroup.position.y=-5;
-		        phoneGroup.position.z=-1;
-		        for (i=0;i<object.material.materials.length;i++) {
-		        	var tC = .75;
-		        	object.material.materials[i].side=2;
-					object.material.materials[i].specular.r=tC;
-					object.material.materials[i].specular.g=tC;
-					object.material.materials[i].specular.b=tC;	
-					object.material.materials[i].emissive.r=.0;
-					object.material.materials[i].emissive.g=.0;
-					object.material.materials[i].emissive.b=.0;
-					object.material.materials[i].shininess=10;	
-		        }
-		       
-		  
-				phoneGroup.add( object );
-				
-		    });
-
-			phoneObject = object;
-
-			phoneGroup.position.z=0;
-			phoneGroup.position.y=-1;
-			phoneGroup.scale.set( 25, 25, 25 );
-			phoneGroup.castShadow = true;
-
-			phone1 = phoneGroup.clone();
-			phone1.position.x=-25;
-			phone1.position.z = -20;
-			
-			phone2 = phoneGroup.clone();
-			phone2.position.x=25;
-			phone2.position.z = -20;
-			// scene.add(phone1);
-			// scene.add(phone2);
-
-			TweenMax.to(phoneGroup.rotation, 1.5, {y:de2ra(0),  ease:Power4.easeOut});
-
-			var tween = TweenMax.to(phoneGroup.position, 3.5,{ y:0, yoyo:true, repeat:-1, onRepeat:repeatFunc, ease:Power2.easeInOut, onComplete:function() {}});
-			setTimeout(function() {
-				var tween = TweenMax.to(phone1.position, 3.5,{ y:0, yoyo:true, repeat:-1, onRepeat:repeatFunc, ease:Power2.easeInOut, onComplete:function() {}});
-			},1500);
-			setTimeout(function() {
-				var tween = TweenMax.to(phone2.position, 3.5,{ y:0, yoyo:true, repeat:-1, onRepeat:repeatFunc, ease:Power2.easeInOut, onComplete:function() {}});
-			},3000);
-			function repeatFunc() {
-
-			}
-		}
-		
-	);
-	macbookGroup = new THREE.Group();
-	var macbookScreenGroup = new THREE.Object3D();
-	loader.load(
-			// resource URL
-			'models/screen.json',
-			// Function when resource is loaded
-			function ( geometry, materials ) {
-				var material = new THREE.MultiMaterial( materials );
-				var object = new THREE.Mesh( geometry, material );
-				// scene.add( object );
-				
-				object.position.y=0;
-				object.position.x=-0.5;
-				object.position.z=0;
-				// object.rotation.y=de2ra(180);
-				
-				object.castShadow=true;
-				object.traverse( function ( child ) {
-		        if ( child instanceof THREE.Mesh ) {
-		            child.geometry.computeVertexNormals();
-		        }
-		        for (i=0;i<object.material.materials.length;i++) {
-		        	var tC = .75;
-		        	object.material.materials[i].side=2;
-					object.material.materials[i].specular.r=tC;
-					object.material.materials[i].specular.g=tC;
-					object.material.materials[i].specular.b=tC;	
-					object.material.materials[i].emissive.r=.0;
-					object.material.materials[i].emissive.g=.0;
-					object.material.materials[i].emissive.b=.0;
-					object.material.materials[i].shininess=1;	
-		        }
-				macbookScreenGroup.add( object );
-		    });
-		}
-	);
-	var desktopMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-	desktopVideo = document.getElementById( 'desktop' );
-	var desktopTexture = new THREE.VideoTexture( desktopVideo );
-	desktopTexture.minFilter = THREE.LinearFilter;
-	desktopTexture.magFilter = THREE.LinearFilter;
-	desktopTexture.format = THREE.RGBFormat;
-	desktopTexture.wrapS = THREE.ClampToEdgeWrapping;
-	desktopTexture.wrapT = THREE.ClampToEdgeWrapping;
-
-	desktopMaterial.map = desktopTexture;
-	
-	var desktopGeometry = new THREE.PlaneGeometry( 8.7, 5.8, 1 );
-
-	var desktopScreen = new THREE.Mesh( desktopGeometry, desktopMaterial );
-	desktopScreen.position.z = -3;
-	desktopScreen.position.y = 3.3;
-	desktopScreen.rotation.x = de2ra(1);
-	macbookScreenGroup.add(desktopScreen);
-
-	var desktopGlass = new THREE.Mesh(desktopGeometry, glassMaterial);
-	desktopGlass.receiveShadow = true;
-	desktopGlass.position.z = -3;
-	desktopGlass.position.y = 3.3;
-	desktopGlass.rotation.x = de2ra(1);
-	desktopGlass.transparent = false;
-	macbookScreenGroup.add(desktopGlass);
-
-	var macbookPivot = new THREE.Object3D();
-	var tempgeometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
-	var tempmaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } );	
-	mesh = new THREE.Mesh( tempgeometry, tempmaterial );
-	macbookPivot.add( mesh );
-	
-	macbookScreenGroup.position.z=3;
-	macbookPivot.add(macbookScreenGroup);
-	macbookPivot.position.z=-3;
-	macbookPivot.rotation.x=de2ra(90);
-	var openLid = TweenMax.to(macbookPivot.rotation, 2, { x:0, ease:Power4.easeInOut});
-	macbookGroup.add(macbookPivot);
-
-	loader.load(
-			// resource URL
-			'models/keyboard.json',
-			// Function when resource is loaded
-			function ( geometry, materials ) {
-				var material = new THREE.MultiMaterial( materials );
-				var object = new THREE.Mesh( geometry, material );
-				// scene.add( object );
-				
-				object.position.y=0;
-				object.position.x=-0.5;
-				object.position.z=0;
-				
-				object.castShadow=true;
-				object.traverse( function ( child ) {
-		        if ( child instanceof THREE.Mesh ) {
-		            child.geometry.computeVertexNormals();
-		        }
-		        for (i=0;i<object.material.materials.length;i++) {
-		        	var tC = .75;
-		        	object.material.materials[i].side=2;
-					object.material.materials[i].specular.r=tC;
-					object.material.materials[i].specular.g=tC;
-					object.material.materials[i].specular.b=tC;	
-					object.material.materials[i].emissive.r=.0;
-					object.material.materials[i].emissive.g=.0;
-					object.material.materials[i].emissive.b=.0;
-					object.material.materials[i].shininess=1.5;	
-		        }
-				macbookGroup.add( object );
-		    });
-		}
-		
-	);
-	loader.load(
-			// resource URL
-			'models/keys.json',
-			// Function when resource is loaded
-			function ( geometry, materials ) {
-				var material = new THREE.MultiMaterial( materials );
-				var object = new THREE.Mesh( geometry, material );
-				// scene.add( object );
-				
-				object.position.y=0;
-				object.position.x=-0.5;
-				object.position.z=0;
-				
-				object.castShadow=true;
-				object.traverse( function ( child ) {
-		        if ( child instanceof THREE.Mesh ) {
-		            child.geometry.computeVertexNormals();
-		        }
-		        for (i=0;i<object.material.materials.length;i++) {
-		        	var tC = .75;
-		        	object.material.materials[i].side=2;
-					object.material.materials[i].specular.r=tC;
-					object.material.materials[i].specular.g=tC;
-					object.material.materials[i].specular.b=tC;	
-					object.material.materials[i].emissive.r=.0;
-					object.material.materials[i].emissive.g=.0;
-					object.material.materials[i].emissive.b=.0;
-					object.material.materials[i].shininess=21;	
-		        }
-				macbookGroup.add( object );
-		    });
-		}
-		
-	);
-	
-
-	// scene.add( phoneGroup );
-
-	macbookGroup.position.y=-10;
-	macbookGroup.position.z=10;
-	macbookGroup.scale.set(4,4,4);
-	scene.add(macbookGroup);
-
-	
-	var tween1 = TweenMax.to(macbookGroup.position, 3.5,{ y:-9, yoyo:true, repeat:-1, ease:Power2.easeInOut, onComplete:function() {}});
-	
-
+		$("section").each(function() {
+			addProject(this,scene);
+		});
+	}
+function setLights() {
+	var groundGeometry = new THREE.PlaneGeometry( 250, 250, 1 );
+	var groundMaterial = new THREE.ShadowMaterial();
+	groundMaterial.opacity = .3;
+	var ground = new THREE.Mesh(groundGeometry, groundMaterial);
+	ground.receiveShadow = true;
+	ground.rotation.x=55;
+	ground.position.y=-20;
+	ground.transparent = true;
+	scene.add(ground);
 
 	var spotLight4 = new THREE.SpotLight( 0xffffff); 
 	spotLight4.castShadow = false;
-	spotLight4.position.set( 20, 35, 20 ); 
+	spotLight4.position.set( 20, 55, 20 ); 
 	spotLight4.target.position.set(00, 15, 0 );
 	spotLight4.shadow.mapSize.height = shadowMapSize;
 	spotLight4.shadow.mapSize.width = shadowMapSize;
-	spotLight4.intensity = 0.20;
+	spotLight4.intensity = 0.15;
 	spotLight4.penumbra = 1.5;
 	// spotLight4.exponent = 15;
 	spotLight4.decay = 2;
@@ -383,37 +100,33 @@ $(function() {
 	// plane.rotation.x = de2ra(90);
 	plane.position.z = -2.7;
 	scene.add( plane );
+}
 
+
+function setEnvironment() {
+	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 2000 );
+	camera.position.z = 65;
+	camera.position.y = 0;
+	scene = new THREE.Scene();
 
 	var myAntialias=true;
-	// if (window.devicePixelRatio>1) {
-	// 	myAntialias=false;
-	// }
-
+	if (window.devicePixelRatio>1) {
+		myAntialias=false;
+	}
 	renderer = new THREE.WebGLRenderer( {alpha: true, antialias: myAntialias });
 	renderer.setClearColor( 0x000000, 0 );
-
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.shadowMap.enabled	= true;
 	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-	
-
-	
-
-
 	container.appendChild( renderer.domElement );
 	// renderer.domElement.style.webkitTransform='scale(.5) translate3d(-50%,-50%,0)';
-	
 	var element = renderer.domElement;
-	
 	window.addEventListener( 'resize', onWindowResize, false );
-	camera.position.set(0, 0, 65);
+	camera.position.set(0, 0, 60);
 	animate();
-
 }
-	
+
 	function onWindowResize() {
 		windowHalfX = window.innerWidth / 2;
 		windowHalfY = window.innerHeight / 2;
